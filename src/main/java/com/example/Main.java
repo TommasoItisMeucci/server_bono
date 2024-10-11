1ff4ba18251ce7ae7a69cc1e2fc84cccdd4d680e
lai2-24 <lai2-24@itismeucci.com>
2024-10-11 09:10:15 +0200

    aggiunta esercizio

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,6 +1,9 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,6 +14,15 @@ public class Main {
         ServerSocket s1 = new ServerSocket(3000);
         Socket s = s1.accept();
         System.out.println("client collegato");
+        
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+        String stringaRicevuta = in.readLine();
+        System.out.println("la stringa ricevuta è: " + stringaRicevuta);
+
+        String stringaMaiuscola = stringaRicevuta.toUpperCase();
+        out.writeBytes(stringaMaiuscola + '\n');
         
 
     }
